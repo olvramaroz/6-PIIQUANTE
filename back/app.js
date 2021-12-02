bbed74febd6d1090f23d64564c0c6698fed69c96
Valerie RAMAROZ <olv.ramaroz@gmail.com>
2021-12-02 08:51:25 +0100

    connexion BDD avec mongoose

--- back/app.js
+++ back/app.js
@@ -1,10 +1,24 @@
 // je fais mes imports
+  // pour créer des applis web avec Node
 const express = require("express");
+  // pour faciliter les inéractions avec la bdd mongoDB
+const mongoose = require("mongoose");
 
 // je fais appel au module Express avec sa fonction
 // le mot-clé app fait souvent référence au module express
 // on peut utiliser un autre nom, mais c'est la convention
 const app = express();
+
+// je me connecte à la BDD
+mongoose.connect('mongodb+srv://new-user:<pwd>@cluster0.m31ja.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// je récupère le body en front sur l'objet request
+// remplace body-parser et analyse donc le corps de la requête
+app.use(express.json());
 
 // j'exporte ce module pour pouvoir le réutiliser ailleurs
 module.exports = app;
@@ -12,18 +26,14 @@ module.exports = app;
 // Avant la route d'API, on ajoute la fonction (middleware) des headers permettant
 // aux deux ports front et end de communiquer entre eux
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); //Permet d'accéder a l'API depuis n'importe quelle origine
+  res.setHeader("Access-Control-Allow-Origin", "*"); // "*" permet d'accéder a l'API depuis n'importe quelle origine
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  ); //Donne l'autorisation d'utiliser certains headers sur l'objet requête
+  ); // autorisation d'utiliser certains headers sur l'objet requête
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  ); //Permet d'envoyer des requêtes avec ces méthodes
-  next(); // Passe l'exécution au middleware suivant
+  ); // permet d'envoyer des requêtes avec ces méthodes
+  next(); // passe l'exécution au middleware suivant
 });
-
-// je récupère le body en front sur l'objet request
-// remplace body-parser et analyse donc le corps de la requête
-app.use(express.json());
